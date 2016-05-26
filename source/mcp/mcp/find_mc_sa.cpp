54c92cce4fa36cb46bbecc4b9d245f70825e8ee7
gx <gx16377@163.com>
2016-05-26 17:11:53 +0800

    sa-a/d-cnum-1
    sim-aneal,add&delete,c(S)=num of clique

--- source/mcp/mcp/find_mc_sa.cpp
+++ source/mcp/mcp/find_mc_sa.cpp
@@ -1,7 +1,7 @@
 #include "mcp.h"
+#include <time.h>
 
 int c(bool s[], int n) {
-	//now a c(S) to select all vertices into S
 	int num = 0;
 	for (int i = 0;i < n;i++) {
 		if (s[i]) num++;
@@ -9,32 +9,93 @@ int c(bool s[], int n) {
 	return n - num;
 }
 
+void compare(bool s[],int n) {
+	int num = 0;
+	for (int i = 0;i < n;i++) {
+		if (s[i]) {
+			num++;
+		}
+	}
+	if (num > mc_count) {
+		cout << num << endl;
+		for (int i = 0;i < n;i++) {
+			mc[i] = s[i];
+		}
+		mc_count = num;
+	}
+}
+
+int latest_add;
+int latest_del;
+
 void find_mc_sa(int n) {
 	int t = n;//gap between initial value of t and range of c(S) effects efficiency a lot
-	bool s[N_v];
+	bool state[N_v];
+	srand(time(0));
 	for (int i = 0;i < n;i++) {
-		s[i] = false;
+		state[i] = false;
 	}
+	state[0] = true;
+	t = c(state, n);
 	int dt;
 	while (true) {
 		int flag = 0;
-		for (int i = 0;i < n;i++) {
-			s[i] = !s[i];
-			dt = c(s, n) - t;
-			float temp = (float)rand() / RAND_MAX;
-			float temp2 = exp(-(float)dt / t) / 2;// divide by 2 to reduce false rate temporarily
-			if (dt <= 0 || temp < temp2) {
-				t = dt + t;
-				flag = 1;
-				break;
+		int i;
+		int add[N_v];
+		int add_num = 0;
+		for (i = 0;i < n;i++) {
+			if (state[i]) break;
+		}
+		for (int j = 0;j < n;j++) {
+			if (adj_matrix[i][j] && !state[j]) {
+				int can_be_added = 1;
+				for (int k = i + 1;k < n;k++) {
+					if (state[k] && !adj_matrix[k][j]) {
+						can_be_added = 0;
+						break;
+					}
+				}
+				if (can_be_added&&j!=latest_del) {
+					state[j] = true;
+					dt = c(state, n) - t;
+					float temp = (float)rand() / RAND_MAX;
+					float temp2 = exp(-(float)dt / t);//should exp(-MAX/t) becomes 0?
+					if (dt <= 0 || temp < temp2) {
+						t = dt + t;
+						flag = 1;
+						latest_add = j;
+						break;
+					}
+					state[j] = false;
+				}
 			}
-			s[i] = !s[i];
+		}
+
+		//when those calculated, change to max-dt state or first proper state
+		//if c(S) = v-num in clique and add_num>0 of course choose first
+
+		if (!flag) {
+			//remove
+			compare(state,n);
+			for (int i = 0;i < n;i++) {
+				if (state[i]&&i!=latest_add) {
+					state[i] = false;
+					dt = c(state, n) - t;
+					float temp = (float)rand() / RAND_MAX;
+					float temp2 = exp(-(float)dt / t)/2;//should exp(-MAX/t) becomes 0?
+					if (dt <= 0 || temp < temp2) {
+						latest_del = i;
+						t = dt + t;
+						flag = 1;
+						break;
+					}
+					state[i] = true;
+				}
+			}
 		}
 		if (!flag) {
-			for (int i = 0;i < n;i++) {
-				mc[i] = s[i];
-				mc_count++;
-			}
+			//compare and add to mc
+			compare(state, n);
 			return;
 		}
 	}
--- source/mcp/mcp/mcp.h
+++ source/mcp/mcp/mcp.h
@@ -1,7 +1,7 @@
 #ifndef Mo_Bai_God_of_Guo
 #define Mo_Bai_God_of_Guo
 
-#define N_v 1000
+#define N_v 4001
 
 #include <iostream>
 #include <fstream>
@@ -14,6 +14,6 @@ extern bool mc[N_v];
 extern int mc_count;
 
 int find_mc_low(int);
-
+void find_mc_sa(int);
 
 #endif
