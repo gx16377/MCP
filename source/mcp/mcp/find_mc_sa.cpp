29d90f34e35d1eaa7baca94cba1be9bdd27ca632
gx <gx16377@163.com>
2016-05-26 20:51:54 +0800

    sa-linklist

--- source/mcp/mcp/find_mc_sa.cpp
+++ source/mcp/mcp/find_mc_sa.cpp
@@ -1,25 +1,29 @@
 #include "mcp.h"
 #include <time.h>
 
-int c(bool s[], int n) {
+typedef struct Vertice {
+	int i;
+	struct Vertice* next;
+} vertice;
+
+int c(vertice *s, int n) {
 	int num = 0;
-	for (int i = 0;i < n;i++) {
-		if (s[i]) num++;
+	for (vertice *v = s;v != nullptr;v = v->next) {
+		num++;
 	}
 	return n - num;
 }
 
-void compare(bool s[],int n) {
+void compare(vertice *s, int n) {
 	int num = 0;
-	for (int i = 0;i < n;i++) {
-		if (s[i]) {
-			num++;
-		}
+	for (vertice *v = s;v != nullptr;v = v->next) {
+		num++;
 	}
 	if (num > mc_count) {
 		cout << num << endl;
-		for (int i = 0;i < n;i++) {
-			mc[i] = s[i];
+		memset(mc, 0, n*sizeof(bool));
+		for (vertice *v = s;v != nullptr;v = v->next) {
+			mc[v->i] = true;
 		}
 		mc_count = num;
 	}
@@ -28,45 +32,44 @@ void compare(bool s[],int n) {
 int latest_add;
 int latest_del;
 
+
+
 void find_mc_sa(int n) {
-	int t = n;//gap between initial value of t and range of c(S) effects efficiency a lot
-	bool state[N_v];
+	//int t = n;//gap between initial value of t and range of c(S) effects efficiency a lot
+	vertice *state;
+	vertice head,temp_v;
+	head.i = 0;
+	head.next = nullptr;
+	state = &head;
+
 	srand(time(0));
-	for (int i = 0;i < n;i++) {
-		state[i] = false;
-	}
-	state[0] = true;
-	t = c(state, n);
+	int t = c(state, n);
 	int dt;
 	while (true) {
 		int flag = 0;
-		int i;
-		int add[N_v];
-		int add_num = 0;
-		for (i = 0;i < n;i++) {
-			if (state[i]) break;
-		}
 		for (int j = 0;j < n;j++) {
-			if (adj_matrix[i][j] && !state[j]) {
-				int can_be_added = 1;
-				for (int k = i + 1;k < n;k++) {
-					if (state[k] && !adj_matrix[k][j]) {
-						can_be_added = 0;
-						break;
-					}
+			int can_be_added = 1;
+			for (vertice *v = state;v != nullptr;v = v->next) {
+				if (!adj_matrix[v->i][j]) {
+					can_be_added = 0;
+					break;
 				}
-				if (can_be_added&&j!=latest_del) {
-					state[j] = true;
-					dt = c(state, n) - t;
-					float temp = (float)rand() / RAND_MAX;
-					float temp2 = exp(-(float)dt / t);//should exp(-MAX/t) becomes 0?
-					if (dt <= 0 || temp < temp2) {
-						t = dt + t;
-						flag = 1;
-						latest_add = j;
-						break;
-					}
-					state[j] = false;
+			}
+			if (can_be_added&&j != latest_del) {
+				temp_v.i = j;
+				temp_v.next = state;
+				dt = c(&temp_v, n) - t;
+				float temp = (float)rand() / RAND_MAX;
+				float temp2 = exp(-(float)dt / t);//should exp(-MAX/t) becomes 0?
+				if (dt <= 0 || temp < temp2) {
+					t = dt + t;
+					flag = 1;
+					latest_add = j;
+					vertice *v = (vertice*)malloc(sizeof(vertice));
+					v->i = j;
+					v->next = state;
+					state = v;
+					break;
 				}
 			}
 		}
@@ -76,25 +79,35 @@ void find_mc_sa(int n) {
 
 		if (!flag) {
 			//remove
-			compare(state,n);
-			for (int i = 0;i < n;i++) {
-				if (state[i]&&i!=latest_add) {
-					state[i] = false;
-					dt = c(state, n) - t;
-					float temp = (float)rand() / RAND_MAX;
-					float temp2 = exp(-(float)dt / t)/2;//should exp(-MAX/t) becomes 0?
-					if (dt <= 0 || temp < temp2) {
-						latest_del = i;
-						t = dt + t;
-						flag = 1;
-						break;
-					}
-					state[i] = true;
+			compare(state, n);
+			vertice *prev = nullptr;
+			for (vertice *v = state;v != nullptr;v = v->next) {
+				if (v->i == latest_add) {
+					prev = v;
+					continue;
 				}
+				vertice *del = v;
+				if (prev == nullptr) {
+					dt = c(state->next, n) - t;
+				} else {
+					prev->next = v->next;
+					dt = c(state, n) - t;
+				}
+				float temp = (float)rand() / RAND_MAX;
+				float temp2 = exp(-(float)dt / t) / 2;//should exp(-MAX/t) becomes 0?
+				if (dt <= 0 || temp < temp2) {
+					latest_del = v->i;
+					t = dt + t;
+					flag = 1;
+					if (prev == nullptr)state=state->next;
+					free(v);
+					break;
+				}
+				if(prev!=nullptr)prev->next = v;
+				prev = v;
 			}
 		}
 		if (!flag) {
-			//compare and add to mc
 			compare(state, n);
 			return;
 		}
